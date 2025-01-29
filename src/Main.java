10c4ede7e789752f2d069dcb1afe02db9fe8b0f5
Aleksey-Kireev <kireev_a_a@mail.ru>
2025-01-30 02:07:33 +0900

    ----- Условные операторы ч.2. ----- Первый коммит.

--- src/Main.java
+++ src/Main.java
@@ -2,38 +2,6 @@
 public class Main {
     public static void main(String[] args) {
 
-//        ----- =====   Задание № 1   ===== -----
-
-        System.out.println();
-        System.out.println("----- =====   Задание № 1   ===== -----");
-        byte clientOS = 0;
-        switch (clientOS) {
-            case 1:
-                System.out.println("Установите версию приложения для Android по ссылке ....");
-                break;
-            case 0:
-                System.out.println("Установите версию приложения для iOS по ссылке ....");
-                break;
-        }
-
-//        ----- =====   Задание № 2   ===== -----
-
-        System.out.println();
-        System.out.println("----- =====   Задание № 2   ===== -----");
-        short clientDeviceYear = 2015;
-        switch (clientOS) {
-            case 1:
-                System.out.println("Установите версию приложения для Android по ссылке ....");
-                break;
-            case 0:
-                System.out.println("Установите версию приложения для iOS по ссылке ....");
-                break;
-        }
-
-
-
-
-//
 
 
 
